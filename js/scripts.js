6357a4a25be0687c68a185348cb205915677a00e
kr <ktrhx4@mail.missouri.edu>
2017-08-11 13:19:47 -0700

    fix bug with console not logging responses, added alert to unchecked response

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,31 @@
+var ruby = 0;
+var php = 0;
+var java = 0;
+var css = 0;
+var csharp = 0;
+
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var nameInput = $("input#name").val();
-    var classInput = $("input#class").val();
-    var experienceInput= $("input#experience").val();
+    event.preventDefault();
+
+    var one = $("input:radio[name=optradio1]:checked").val();
+
+    if (one==undefined) {
+      alert("Please select a response for each question");
+    } if (one == 'ruby') {
+      ruby += 1;
+    } else if (one == 'php') {
+      php += 1;
+    } else if (one == 'java') {
+      java += 1;
+    } else if (one == 'css') {
+      css += 1;
+    } else if (one == 'csharp') {
+      csharp += 1;
+    }
 
     $(".name").text(nameInput);
-    $(".class").text(classInput);
-    $(".experience").text(experienceInput);
 
-    event.preventDefault();
   });
 });

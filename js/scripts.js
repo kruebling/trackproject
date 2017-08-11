7dc7c59049820d911fcc99e605bbb1364fd4d831
kr <ktrhx4@mail.missouri.edu>
2017-08-11 16:48:16 -0700

    submit broken site for code review. Discovered massive bug with form id error, will continue working

--- js/scripts.js
+++ js/scripts.js
@@ -15,8 +15,8 @@ $(document).ready(function() {
     var one = $("input:radio[name=optradio1]:checked").val();
     var two = $("input:radio[name=optradio2]:checked").val();
     var three = $("input:radio[name=optradio3]:checked").val();
-    var four = $("input:radio[name=optradio3]:checked").val();
-    var five = $("input:radio[name=optradio1]:checked").val();
+    var four = $("input:radio[name=optradio4]:checked").val();
+    var five = $("input:radio[name=optradio5]:checked").val();
 
     if(nameInput.match(/^\d+$/)) {
       alert("Please enter only letters");
@@ -24,6 +24,7 @@ $(document).ready(function() {
 
    }  if (one==undefined || two==undefined || three==undefined || four==undefined || five==undefined) {
       alert("Please select a response for each question");
+      return;
 
     } if (one == 'ruby') {
       ruby += 1;
@@ -34,6 +35,46 @@ $(document).ready(function() {
     } else if (one == 'css') {
       css += 1;
     } else if (one == 'csharp') {
+      csharp += 1;
+    } if (two == 'ruby') {
+      ruby += 1;
+    } else if (two == 'php') {
+      php += 1;
+    } else if (two == 'java') {
+      java += 1;
+    } else if (two == 'css') {
+      css += 1;
+    } else if (two == 'csharp') {
+      csharp += 1;
+    } if (three == 'ruby') {
+      ruby += 1;
+    } else if (three == 'php') {
+      php += 1;
+    } else if (three == 'java') {
+      java += 1;
+    } else if (three == 'css') {
+      css += 1;
+    } else if (three == 'csharp') {
+      csharp += 1;
+    } if (four == 'ruby') {
+      ruby += 1;
+    } else if (four == 'php') {
+      php += 1;
+    } else if (four == 'java') {
+      java += 1;
+    } else if (four == 'css') {
+      css += 1;
+    } else if (four == 'csharp') {
+      csharp += 1;
+    } if (five == 'ruby') {
+      ruby += 1;
+    } else if (five == 'php') {
+      php += 1;
+    } else if (five == 'java') {
+      java += 1;
+    } else if (five == 'css') {
+      css += 1;
+    } else if (five == 'csharp') {
       csharp += 1;
     }
   });

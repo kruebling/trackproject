b1d4fcd170902c8aa0e79d619967ed2bbba4c5af
kr <ktrhx4@mail.missouri.edu>
2017-08-11 18:10:32 -0700

    fixed bug prevent js from collecting input information from forms

--- js/scripts.js
+++ js/scripts.js
@@ -6,26 +6,18 @@ var csharp = 0;
 
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-
     var nameInput = $("input#name").val();
-    event.preventDefault();
-
     $(".name").text(nameInput);
 
     var one = $("input:radio[name=optradio1]:checked").val();
-    var two = $("input:radio[name=optradio2]:checked").val();
-    var three = $("input:radio[name=optradio3]:checked").val();
-    var four = $("input:radio[name=optradio4]:checked").val();
-    var five = $("input:radio[name=optradio5]:checked").val();
 
     if(nameInput.match(/^\d+$/)) {
       alert("Please enter only letters");
       return;
-
-   }  if (one==undefined || two==undefined || three==undefined || four==undefined || five==undefined) {
-      alert("Please select a response for each question");
-      return;
-
+    }
+    if (one==undefined) {
+    alert("Please select a response for each question");
+    return;
     } if (one == 'ruby') {
       ruby += 1;
     } else if (one == 'php') {
@@ -36,7 +28,23 @@ $(document).ready(function() {
       css += 1;
     } else if (one == 'csharp') {
       csharp += 1;
-    } if (two == 'ruby') {
+    }
+
+    $("#formOne").hide(800);
+    $("#formTwo").show(800);
+    $("h2").hide();
+    event.preventDefault();
+  });
+});
+$(document).ready(function() {
+  $("#formTwo").submit(function(event) {
+
+    var two = $("input:radio[name=optradio2]:checked").val();
+
+    if (two==undefined) {
+    alert("Please select a response for each question");
+    return;
+  } if (two == 'ruby') {
       ruby += 1;
     } else if (two == 'php') {
       php += 1;
@@ -46,7 +54,24 @@ $(document).ready(function() {
       css += 1;
     } else if (two == 'csharp') {
       csharp += 1;
-    } if (three == 'ruby') {
+    }
+    $("#formOne").hide(800);
+    $("#formTwo").hide(800);
+    $("#formThree").show(800);
+    $("h2").hide();
+    event.preventDefault();
+  });
+});
+
+$(document).ready(function() {
+  $("#formThree").submit(function(event) {
+
+    var three = $("input:radio[name=optradio3]:checked").val();
+
+    if (three==undefined) {
+    alert("Please select a response for each question");
+    return;
+  } if (three == 'ruby') {
       ruby += 1;
     } else if (three == 'php') {
       php += 1;
@@ -56,7 +81,25 @@ $(document).ready(function() {
       css += 1;
     } else if (three == 'csharp') {
       csharp += 1;
-    } if (four == 'ruby') {
+    }
+    $("#formOne").hide(800);
+    $("#formTwo").hide(800);
+    $("#formThree").hide(800);
+    $("#formFour").show(800);
+    $("h2").hide();
+    event.preventDefault();
+  });
+});
+
+$(document).ready(function() {
+  $("#formFour").submit(function(event) {
+
+    var four = $("input:radio[name=optradio4]:checked").val();
+
+    if (four==undefined) {
+    alert("Please select a response for each question");
+    return;
+  } if (four == 'ruby') {
       ruby += 1;
     } else if (four == 'php') {
       php += 1;
@@ -66,7 +109,26 @@ $(document).ready(function() {
       css += 1;
     } else if (four == 'csharp') {
       csharp += 1;
-    } if (five == 'ruby') {
+    }
+    $("#formOne").hide(800);
+    $("#formTwo").hide(800);
+    $("#formThree").hide(800);
+    $("#formFour").hide(800);
+    $("#formFive").show(800);
+    $("h2").hide();
+    event.preventDefault();
+  });
+});
+
+$(document).ready(function() {
+  $("#formFive").submit(function(event) {
+
+    var five = $("input:radio[name=optradio5]:checked").val();
+
+    if (five==undefined) {
+    alert("Please select a response for each question");
+    return;
+  } if (five == 'ruby') {
       ruby += 1;
     } else if (five == 'php') {
       php += 1;
@@ -77,46 +139,12 @@ $(document).ready(function() {
     } else if (five == 'csharp') {
       csharp += 1;
     }
-  });
-});
-
-$(document).ready(function() {
-  $("#formOne").submit(function(event) {
     $("#formOne").hide(800);
-    $("#formTwo").show(800);
+    $("#formTwo").hide(800);
+    $("#formThree").hide(800);
+    $("#formFour").hide(800);
+    $("#formFive").hide(800);
     $("h2").hide();
     event.preventDefault();
-    $("#formTwo").submit(function(event) {
-      $("#formTwo").hide(800);
-      $("#formThree").show(800);
-      $("h2").hide();
-      event.preventDefault();
-      $("#formThree").submit(function(event) {
-        $("#formOne").hide(800);
-        $("#formTwo").hide(800);
-        $("#formThree").hide(800);
-        $("#formFour").show(800);
-        $("h2").hide();
-        event.preventDefault();
-        $("#formFour").submit(function(event) {
-          $("#formOne").hide(800);
-          $("#formTwo").hide(800);
-          $("#formThree").hide(800);
-          $("#formFour").hide(800);
-          $("#formFive").show(800);
-          $("h2").hide();
-          event.preventDefault();
-          $("#formFive").submit(function(event) {
-            $("#formOne").hide(800);
-            $("#formTwo").hide(800);
-            $("#formThree").hide(800);
-            $("#formFour").hide(800);
-            $("#formFive").hide(800);
-            $("h2").hide();
-            event.preventDefault();
-          });
-        });
-      });
-    });
   });
 });
